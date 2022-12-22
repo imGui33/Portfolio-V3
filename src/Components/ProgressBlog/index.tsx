3aa405b919b70bf93ee727e92940a0d7ba350c0c
imGuii <69533374+imGui33@users.noreply.github.com>
2022-12-22 04:36:39 -0400

    a

--- src/Components/ProgressBlog/index.tsx
+++ src/Components/ProgressBlog/index.tsx
@@ -4,6 +4,16 @@ import { ProgressContainer } from "./style";
 
 const ProgressBlog: React.FC = () => {
     const [scrollValue, setScrollValue] = useState(0)
+    const showProgress = () => {
+        const scrollTop = document.documentElement.scrollTop
+        const height = document.documentElement.scrollHeight - document.documentElement.clientHeight
+        const value = (scrollTop - height) * 100
+        setScrollValue(value)
+    }
+    useEffect(() => {
+        window.addEventListener('scroll', showProgress)
+        return () => window.removeEventListener('scroll', showProgress)
+    })
   return (
     <ProgressContainer>
       <div className="progress-line" style={{width:`${scrollValue}%`}}></div>
